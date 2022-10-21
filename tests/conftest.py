483816bc5bdcce87a1d8ee46edd7ba07470e5637
Torsten Glunde <torsten.glunde@alligator-company.com>
2022-10-21 11:44:51 +0200

    formatting and pyenv 3.7.2 added

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,14 @@ import pytest
 
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
+
 @pytest.fixture(scope="class")
 def dbt_profile_target():
     return {
-        'type': 'exasol',
-        'threads': 1,
-        'dsn': os.getenv('DBT_DSN'),
-        'user': os.getenv('DBT_USER'),
-        'pass': os.getenv('DBT_PASS'),
-        'dbname': 'DB',
+        "type": "exasol",
+        "threads": 1,
+        "dsn": os.getenv("DBT_DSN"),
+        "user": os.getenv("DBT_USER"),
+        "pass": os.getenv("DBT_PASS"),
+        "dbname": "DB",
     }
