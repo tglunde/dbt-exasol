affca4f8427b7feaeb8413e16ce3201de0afb99b
ilikutle <ilija.kutle@alligator-company.com>
2022-11-16 16:40:55 +0100

    added .env load to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,21 @@
 import os
-
+from pathlib import Path
+from dotenv import load_dotenv
 import pytest
 
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
+dotenv_path = Path('test.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 @pytest.fixture(scope="class")
 def dbt_profile_target():
     return {
-        "type": "exasol",
-        "threads": 1,
-        "dsn": os.getenv("DBT_DSN"),
-        "user": os.getenv("DBT_USER"),
-        "pass": os.getenv("DBT_PASS"),
-        "dbname": "DB",
-    }
+       "type": "exasol",
+       "threads": 1,
+       "dsn": os.getenv('DBT_DSN',"localhost:8563"),
+       "user": os.getenv('DBT_USER',"sys"),
+       "pass": os.getenv('DBT_PASS',"exasol"),
+       "dbname": "DB",
+   }
+
