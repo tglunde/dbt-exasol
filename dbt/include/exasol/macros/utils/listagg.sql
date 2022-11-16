50b8ea49994591107699614b2cf96700f5903d13
ilikutle <ilija.kutle@alligator-company.com>
2022-11-16 16:37:26 +0100

    existing utils edit, last_day added

--- dbt/include/exasol/macros/utils/listagg.sql
+++ dbt/include/exasol/macros/utils/listagg.sql
@@ -1,6 +1,6 @@
 {% macro exasol__listagg(measure, delimiter_text, order_by_clause, limit_num) -%}
     {% if limit_num -%}
-    {{ exceptions.raise_compiler_error("`limit_num` parameter is not supported on Exasol!") }}
+        {{ exceptions.raise_compiler_error("`limit_num` parameter is not supported on Exasol!") }}
     {% endif -%}
 
     listagg(
