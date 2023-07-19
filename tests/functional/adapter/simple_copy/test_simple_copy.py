b0f0707bc84fb8404f8d2bb8aeddccb10644d779
ilikutle <ilija.kutle@alligator-company.com>
2023-07-19 11:32:15 +0200

    simple copy tests fix

--- tests/functional/adapter/simple_copy/test_simple_copy.py
+++ tests/functional/adapter/simple_copy/test_simple_copy.py
@@ -10,22 +10,9 @@ from dbt.tests.adapter.simple_copy.test_simple_copy import (
     EmptyModelsArentRunBase,
 )
 
-from tests.functional.adapter.simple_copy.fixtures import (
-    _MODELS__INCREMENTAL_OVERWRITE,
-    _MODELS__INCREMENTAL_UPDATE_COLS,
-    _SEEDS__SEED_MERGE_EXPECTED,
-    _SEEDS__SEED_MERGE_INITIAL,
-    _SEEDS__SEED_MERGE_UPDATE,
-    _SEEDS__SEED_UPDATE,
-    _TESTS__GET_RELATION_QUOTING,
-)
 
 
 class TestSimpleCopyBase(SimpleCopyBase):
-    @pytest.fixture(scope="class")
-    def tests(self):
-        return {"get_relation_test.sql": _TESTS__GET_RELATION_QUOTING}
-
     @pytest.mark.xfail
     def test_simple_copy_with_materialized_views(self, project):
         pass
